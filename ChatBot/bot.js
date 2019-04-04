b3ceb70a1019fe86c3b150e11e80688173b7313a
Zachary Johnson <ztj1@iastate.edu>
2019-04-04 18:41:10 -0500

    Moved script to head

--- ChatBot/bot.js
+++ ChatBot/bot.js
@@ -3,3 +3,11 @@ var response = '<h1>Hello, I am a bot. Ask me a question</h1>';
 var output = document.getElementById('output');
 
 output.innerHTML = response;
+
+function bot() {
+    var input = document.getElementById('input').nodeValue;
+    console.log(input);
+    if (input.substring(0,5) == 'Hello') {
+        output.innerHTML = 'Hi!';
+    }
+}
