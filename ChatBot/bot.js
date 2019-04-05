2116c775e9ef80c0fa4e11a82d609f34d49ca467
Zachary Johnson <ztj1@iastate.edu>
2019-04-04 19:09:28 -0500

    Fixed JavaScript

--- ChatBot/bot.js
+++ ChatBot/bot.js
@@ -5,7 +5,7 @@ var output = document.getElementById('output');
 output.innerHTML = response;
 
 function bot() {
-    var input = document.getElementById('input').nodeValue;
+    var input = document.getElementById('input').value;
     console.log(input);
     if (input.substring(0,5) == 'Hello') {
         output.innerHTML= '<h1>Hi!<h1>';
